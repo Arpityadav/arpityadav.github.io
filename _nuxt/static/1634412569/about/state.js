ab36786c864b003e21b20a0a606a197bcf1f13c0
Arpit <arpit.yadav1912@gmail.com>
2021-10-17 01:01:13 +0530

    Add files via upload

---
+++ _nuxt/static/1634412569/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fdist\u002F_nuxt\u002Fstatic\u002F1634412569",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{app:{basePath:"\u002Fdist\u002F",assetsPath:"\u002Fdist\u002F_nuxt\u002F",cdnURL:a}}}}(null));
